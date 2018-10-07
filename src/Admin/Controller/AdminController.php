c133d4f3ecbeb858f0a1d8cd2a58c6d880d59545
Adam Rogers <adam.rogers@accordgroup.co.uk>
2018-10-07 23:40:23 +0100

    sorted out login with session

--- src/Admin/Controller/AdminController.php
+++ src/Admin/Controller/AdminController.php
@@ -1,19 +1,22 @@
 <?php 
-
 namespace Src\Admin\Controller;
-
 use Src\Admin\Parameters\AdminSettings;
-
 use PDO;
-
+session_start();
 class AdminController {
 
-    public function index($mysqli, $twig) {
-
+    private $twig;
+    public function __construct(){
         $settings = new AdminSettings();
-        $settings->setTemplating("twig", "../src/Site/Twig", "");
+        $settings->setTemplating("twig", "../src/Admin/Twig", "");
         $twig = $settings->getTemplating();
-        
+
+        if( !isset( $_SESSION['login'] ) ) {
+            echo $twig->render('admin/login.html.twig', array('name' => 'Fabien'));
+        }
+    }
+    public function index($mysqli, $twig) {
+       
         echo $twig->render('homepage/index.html.twig', array('name' => 'Fabien'));
     }
 
